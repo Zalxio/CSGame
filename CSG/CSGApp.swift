b2cf8393e1b3b0422f3717d8d966f15c5a8fbb2c
Zalxio <dyl_974@icloud.com>
2023-02-02 11:30:24 +0400

    test

---
+++ CSG/CSGApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSGApp.swift
+//  CSG
+//
+//  Created by Dylan Cherrier on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CSGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSG/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CSG
+//
+//  Created by Dylan Cherrier on 02/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
